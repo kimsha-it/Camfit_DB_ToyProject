2840adb203ab36a805fb3fb16c1d18b4671b043d
Yimin2 <gownfla14@naver.com>
2025-10-29 17:22:45 +0900

    폴더구조 수정

---
+++ 이민구/campit.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE product
+	CHARACTER SET UTF8MB4
+    COLLATE utf8mb_unicode_ci;
+
+USE campit;
+
+CREATE TABLE product (
+	product_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_name INT 
+);
